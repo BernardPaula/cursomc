0563d997e90c95f99b4a86ca2edf555be07b109d
BeranrdPaula <bernardstudioart@gmail.com>
2022-02-07 09:05:44 -0200

    REST testado

--- src/main/java/com/bernardpaula/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/bernardpaula/cursomc/resources/CategoriaResource.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/categorias")
+@RestController                              //definiu como controlador rest
+@RequestMapping(value = "/categorias")       //mapeou o end poit
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)   //executa este metodo
 	public String listar() {
 		return "REST está funcionando!";
 	}
 
 }
+
+
